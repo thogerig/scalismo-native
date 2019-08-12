5bd88188624e304f911185979441ff730f215dd0
Marcel Luethi <marcel.luethi@unibas.ch>
2019-08-12 21:06:57 +0200

    added mac and win64 natives

---
+++ implementation/src/main/resources/scalismo/support/nativelibs/vtk8/mac_x86_64/cmake/vtk-8.2/Modules/vtkWrappingJava.cmake
@@ -0,0 +1,12 @@
+set(vtkWrappingJava_LOADED 1)
+set(vtkWrappingJava_DEPENDS "vtkCommonCore;vtkWrappingTools")
+set(vtkWrappingJava_LIBRARIES "vtkWrappingJava")
+set(vtkWrappingJava_INCLUDE_DIRS "${VTK_INSTALL_PREFIX}/include/vtk-8.2")
+set(vtkWrappingJava_LIBRARY_DIRS "")
+set(vtkWrappingJava_RUNTIME_LIBRARY_DIRS "${VTK_INSTALL_PREFIX}/lib")
+set(vtkWrappingJava_WRAP_HIERARCHY_FILE "${CMAKE_CURRENT_LIST_DIR}/vtkWrappingJavaHierarchy.txt")
+set(vtkWrappingJava_KIT "")
+set(vtkWrappingJava_TARGETS_FILE "")
+set(vtkWrappingJava_EXCLUDE_FROM_WRAPPING 1)
+
+
